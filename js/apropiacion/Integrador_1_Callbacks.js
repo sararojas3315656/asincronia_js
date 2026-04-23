5abd1d171936af9382209d7bbee3579b5cf77d26
josuesolano21 <josues230@hotmail.com>
2026-04-23 13:56:32 -0500

    Cambios en la Estructura del Repo, dividido ejercicio integrador 1 en 3 archivos

---
+++ js/apropiacion/Integrador_1_Callbacks.js
@@ -0,0 +1,8 @@
+/*Ejercicio integrador 1 Callbacks:
+Simular un proceso de “consulta de usuario”, que requiere:
+1. “Buscar usuario” (promesa de 1 segundo)
+2. “Consultar permisos” (promesa de 2 segundos)
+3. “Generar reporte final” (promesa de 1 segundo)
+Realizarlo con :
+• Con callbacks
+Meta: identificar ventajas y desventajas reales de cada técnica.*/
--- js/apropiacion/Integrador_1_async_await.js
+++ js/apropiacion/Integrador_1_async_await.js
@@ -1,10 +1,8 @@
-/*Ejercicio integrador 1:
+/*Ejercicio integrador 1 Async/Await:
 Simular un proceso de “consulta de usuario”, que requiere:
 1. “Buscar usuario” (promesa de 1 segundo)
 2. “Consultar permisos” (promesa de 2 segundos)
 3. “Generar reporte final” (promesa de 1 segundo)
-Realizarlo en tres versiones:
-• Con callbacks
-• Con promesas
+Realizarlo con:
 • Con async/await
 Meta: identificar ventajas y desventajas reales de cada técnica.*/
