122c50ed32ecf76e33bd4b1e4ca78250f4bb0e54
sararojas3315656 <saresro04@gmail.com>
2026-04-27 16:17:08 -0600

    Ejercicio 2 realizado by Sara

--- js/apropiacion/ejercicio_2.js
+++ js/apropiacion/ejercicio_2.js
@@ -3,3 +3,10 @@ Ejercicio:
 Crea un ciclo muy grande (por ejemplo, uno que cuente hasta millones) y observa cómo afecta la
 ejecución del programa.
 Meta: evidenciar cómo una tarea pesada bloquea el hilo principal.*/
+
+console.log("Inicio");
+
+for (let i = 0; i < 100_000_000_000; i++) {
+}
+
+console.log("Fin");
