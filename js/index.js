e7d0056926a9cf0e25c415059bf51bc348699ab6
sararojas3315656 <saresro04@gmail.com>
2026-04-27 16:26:22 -0600

    Ejercicio 3 realizado by Sara

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
 /* archivo barril para exportar todo lo que hay en la carpeta js */
 
 export { ejecutarProceso } from "./apropiacion/ejercicio_6.js";
+
+
+export { procesarPedido } from "./js/apropiacion/ejercicio_3.js";
